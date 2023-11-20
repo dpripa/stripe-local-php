1671a6e11bcd539109d131f8c8bb1a45bf727a58
Dmitry Pripa <dmitrypripa@gmail.com>
2023-11-20 21:00:20 +0200

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Stripe Local PHP
+Redirect Stripe requests for local development in a PHP environment.
+
+### Run the following CLI command:
+- Install dependencies:
+```sh
+composer install
+```
+- Login to Stripe:
+```sh
+stripe login
+```
+- Start listener:
+```sh
+stripe listen --forward-to stripe.local
+```
+Done, requests will already be redirected automatically. Have a fun development!
---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+use Stripe\Exception\SignatureVerificationException;
+use Stripe\StripeClient;
+use Stripe\Webhook;
+
+if (empty($_SERVER['HTTP_STRIPE_SIGNATURE'])) {
+    exit('Please read the README.md');
+}
+
+require 'vendor/autoload.php';
+
+$stripe = new StripeClient('sk_test_...');
+$env = file_exists('.env') ? parse_ini_file('.env') : [];
+
+if (empty($env['ENDPOINT_SECRET'])) {
+    echo 'Error: create and populate the .env file using the .env.example as a reference';
+    exit();
+}
+
+$endpoint_secret = $env['ENDPOINT_SECRET'];
+$payload = @file_get_contents('php://input');
+$sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
+$event = null;
+
+try {
+    $event = Webhook::constructEvent(
+        $payload, $sig_header, $endpoint_secret
+    );
+
+} catch(\UnexpectedValueException|SignatureVerificationException $e) {
+    http_response_code(400);
+    exit();
+}
+
+switch ($event->type) {
+    case 'payment_intent.succeeded':
+        $paymentIntent = $event->data->object;
+        break;
+
+    default:
+        echo 'Received unknown event type ' . $event->type;
+}
